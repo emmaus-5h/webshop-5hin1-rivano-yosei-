8dad547be8bf8d88cd472d9be0c04121f3ce3205
yosei <93277357+Yosei1020@users.noreply.github.com>
2023-02-19 19:24:02 +0000

    ff

--- db/create.sql
+++ db/create.sql
@@ -14,24 +14,13 @@ CREATE TABLE products (
 
 CREATE TABLE products_bundle (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  products_id INTEGER
-);
-
-CREATE TABLE author (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
   name VARCHAR(255),
+  oppervlakte TEXT,
+  inwoners TEXT,
+  continent TEXT,
+  code VARCHAR(15),
+  price NUMERIC(10, 2)
 );
-
-CREATE TABLE productReview (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  products_id INTEGER,
-  review_id INTEGER,
-  review TEXT,
-  grade NUMERIC(10, 2)
-);
-
-
-
 --
 -- populate with data
 --
@@ -56,7 +45,7 @@ insert into products (name, oppervlakte, inwoners, continent, code, price) value
 
 insert into products (name, oppervlakte, inwoners, continent, code, price) values ('Turkije', '783.562 km²', '84,78 miljoen inwoners', 'Midden-Oosten', '2657688176-X', 8.732);
 
-insert into products (name, oppervlakte, inwoners, continent, code, price) values ('Canada', '9.985.000 km²', '38,25 miljoen inwoners', 'Noord-Amerika', '1738065049-X', 39,669);
+insert into products (name, oppervlakte, inwoners, continent, code, price) values ('Canada', '9.985.000 km²', '38,25 miljoen inwoners', 'Noord-Amerika', '1738065049-X', 39.669);
 
 insert into products (name, oppervlakte, inwoners, continent, code, price) values ('Verenigde Staten', '9.834.000 km²', '331,9 miljoen inwoners', 'Noord-Amerika', '492662523-X', 46.381);
 
@@ -85,18 +74,48 @@ insert into products (name, oppervlakte, inwoners, continent, code, price) value
 insert into products (name, oppervlakte, inwoners, continent, code, price) values ('Madagaskar', '587.041 km²', '28,92 miljoen inwoners', 'Afrika', '492662523-7', 412);
 
 /* landen bundle */
-insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Europa bundle', 'goeie', '492662523-7', 0);
+insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Europa bundle','111','111', 'goeie', '492662523-7', 0);
 
-insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Amerika bundle', 'trump', '492662523-7', 0);
+insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Amerika bundle','222','222', 'trump', '492662523-7', 0);
 
-insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Afrika bundle', 'goeie', '492662523-7', 0);
+insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Afrika bundle','333','333', 'goeie', '492662523-7', 0);
 
-insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Arabië bundle', 'goeie', '492662523-7', 0);
+insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Arabië bundle','444','444', 'goeie', '492662523-7', 0);
 
-insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Azië bundle', 'goeie', '492662523-7', 0);
+insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Azië bundle', 'goeie','555','555', '492662523-7', 0);
 
-insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Wereldmacht bundle', 'Putin Trump Kim winnie de poe', '492662523-7', 0);
+insert into products_bundle (name, oppervlakte, inwoners, continent, code, price) values ('Wereldmacht bundle','666', '666', 'Putin Trump Kim winnie de poe', '492662523-7', 0);
 
-SELECT * FROM products_bundle, products
-  WHERE products_bundle.products.id = products.id;
+
+
 /* review */
+CREATE TABLE productReview (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  persons_id INTEGER,
+  products_id INTEGER,
+  grade NUMERIC(10, 2)
+);
+
+
+
+/* personen */
+
+CREATE TABLE persons (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  name VARCHAR(255)
+);
+
+insert into persons (name) values ('Sami');
+insert into persons (name) values ('Shurely');
+insert into persons (name) values ('Rivano');
+insert into persons (name) values ('Joshua');
+insert into persons (name) values ('Maaike');
+
+
+
+
+insert into productReview (persons_id, products_id, grade) values (1, 1, '10/10');
+insert into productReview (persons_id, products_id, grade) values (2, 4, '12/10');
+insert into productReview (persons_id, products_id, grade) values (3, 7, '5/10');
+insert into productReview (persons_id, products_id, grade) values (4, 2, '7/10');
+insert into productReview (persons_id, products_id, grade) values (5, 3, '2/10');
